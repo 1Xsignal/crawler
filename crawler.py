156b1c2753c96603c46752a53c1a0e3f8405e527
SajadDz <signalprojectx@gmail.com>
2024-12-11 03:57:31 +0330

    get channel id in telegram

--- crawler.py
+++ crawler.py
@@ -1,22 +1,24 @@
+from telethon.sync import TelegramClient
 from session import Session
-from telethon import TelegramClient, events
-import asyncio
-
-channels=['@altsignalssupport','@altsignalssupport_low']
-
-client=TelegramClient(Session.session_name,Session.api_id,Session.api_hash)
-
-async def monitor_channel(channel_username):
-    print(f"Listening to channel: {channel_username}")
-    async for message in client.iter_messages(channel_username):
-        print(f"Channel: {channel_username} | Message ID: {message.id} | Text: {message.text}")
 
 
-async def main():
+def get_chanell_of_telegram(session_name, api_id, api_hash):
+    with TelegramClient(session_name,api_id, api_hash) as client:
+        print("Logged in successfully!")
 
-    tasks = [monitor_channel(channel) for channel in channels]
-    await asyncio.gather(*tasks)
+        dialogs = client.get_dialogs()
+
+        private_channels = [
+            (dialog.entity.title, dialog.entity.id)
+            for dialog in dialogs
+            if dialog.is_channel and dialog.entity.broadcast
+        ]
+
+        print("Private Channels:")
+        channel_dict={title:channel_id for title, channel_id in private_channels}
+        return channel_dict
 
 
-with client:
-    client.loop.run_until_complete(main())
+print(get_chanell_of_telegram(Session.session_name,Session.api_id,Session.api_hash))
+
+
